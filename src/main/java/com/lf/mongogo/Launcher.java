118adff6ca36963b20956c1f0c6d5c83c3e198e5
YemingLakeForest <ansonn2005@hotmail.com>
2015-11-30 23:29:55 +0000

    Added Spark and Freemarker stuffs.

--- src/main/java/com/lf/mongogo/Launcher.java
+++ src/main/java/com/lf/mongogo/Launcher.java
@@ -1,9 +1,14 @@
 package com.lf.mongogo;
 
-import spark.Request;
-import spark.Response;
-import spark.Route;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
 import spark.Spark;
+
+import java.io.IOException;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Hello world!
@@ -13,10 +18,27 @@ public class Launcher {
 
     public static void main(String[] args) {
 
-        Spark.get("/", new Route() {
-            public Object handle(Request request, Response response) throws Exception {
-                return "Fuck it..";
+        Configuration configuration = new Configuration();
+        configuration.setClassForTemplateLoading(Launcher.class, "/");
+
+
+        Spark.get("/mongogo", (request, response) -> {
+            StringWriter writer = new StringWriter();
+
+            try {
+                Template template = configuration.getTemplate("hello.ftl");
+
+
+                Map<String, Object> helloMap = new HashMap<>();
+                helloMap.put("name", "Yeming");
+
+                template.process(helloMap, writer);
+
+            } catch (IOException |TemplateException e) {
+                e.printStackTrace();
             }
+
+            return writer;
         });
 
     }
