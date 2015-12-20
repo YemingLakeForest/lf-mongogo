3187be02a6f4f5ecd740c016cb520a3d2113b09a
YemingLakeForest <ansonn2005@hotmail.com>
2015-12-20 15:29:25 +0000

    Homework for 2.3 - Finished.

--- src/main/java/com/lf/mongogo/m101j/hw23/Homework23.java
+++ src/main/java/com/lf/mongogo/m101j/hw23/Homework23.java
@@ -9,30 +9,52 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 import org.bson.codecs.DocumentCodec;
 import org.bson.codecs.EncoderContext;
+import org.bson.conversions.Bson;
 import org.bson.json.JsonMode;
 import org.bson.json.JsonWriter;
 import org.bson.json.JsonWriterSettings;
+import org.bson.types.ObjectId;
 
 import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.mongodb.client.model.Sorts.ascending;
+import static com.mongodb.client.model.Sorts.orderBy;
+
 public class Homework23 {
 
 
     public static void main(String[] args) {
-        MongoClientOptions options = MongoClientOptions.builder().connectionsPerHost(100).build();
 
+        MongoClientOptions options = MongoClientOptions.builder().connectionsPerHost(100).build();
         MongoClient client = new MongoClient(new ServerAddress(), options);
 
         MongoDatabase db = client.getDatabase("students").withReadPreference(ReadPreference.secondary());
-
         MongoCollection collection = db.getCollection("grades");
 
-        List<Document> all = (List<Document>) collection.find().into(new ArrayList<Document>());
+        Bson filter = new Document("type", "homework");
+        Bson sort = orderBy(ascending("student_id"), ascending("score"));
+
+        List<Document> all = (List<Document>) collection
+                .find(filter)
+                .sort(sort)
+                .into(new ArrayList<Document>());
+
+        int id = -1;
 
         for (Document document : all) {
-            printJson(document);
+
+            int studentId = (int) document.get("student_id");
+
+            if (id != studentId) {
+                System.out.println("removing this record!!!");
+                printJson(document);
+                ObjectId objectId = document.getObjectId("_id");
+
+                collection.deleteOne(new Document("_id", objectId));
+            }
+            id = studentId;
         }
     }
 
